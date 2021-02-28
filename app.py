43a2bb252449851714c6c8a0be372623af40ec38
Muhammad Sami Khanday <61789670+m-samik@users.noreply.github.com>
2021-02-28 13:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    return "<img src ="https://myimagestorage.blob.core.windows.net/mycontainerimages/mains.jpg">"
     return "<center><h1>Hello !! its <b> Muhammad Sami</b> </h1> </center>"
