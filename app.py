787f609705fd77c68a3c9cb5fa73bb21671d7ae0
Muhammad Sami Khanday <61789670+m-samik@users.noreply.github.com>
2021-02-28 18:15:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-"""from flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<center><h1>Hello !! its <b>Muhammad Sami</b></h1><br><img src='https://myimagestorage.blob.core.windows.net/mycontainerimages/mains.jpg' width=100px height=120px>" """
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('coming-soon.html')
+    return "<center><h1>Hello !! its <b>Muhammad Sami</b></h1><br><img src='https://myimagestorage.blob.core.windows.net/mycontainerimages/mains.jpg' width=100px height=120px>" 
 
 
