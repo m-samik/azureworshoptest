fe053ebdcea70ea789a80f771563fa2e7308f4f5
Muhammad Sami Khanday <61789670+m-samik@users.noreply.github.com>
2021-02-28 18:07:59 +0530

    added coming soon

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+"""from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<center><h1>Hello !! its <b>Muhammad Sami</b></h1><br><img src='https://myimagestorage.blob.core.windows.net/mycontainerimages/mains.jpg' width=100px height=120px>" 
+    return "<center><h1>Hello !! its <b>Muhammad Sami</b></h1><br><img src='https://myimagestorage.blob.core.windows.net/mycontainerimages/mains.jpg' width=100px height=120px>" """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('coming-soon.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
